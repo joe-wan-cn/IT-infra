0464aa123c2641e327bb0bd1bd10e1cab937efdd
joe-wan-cn <121702378+joe-wan-cn@users.noreply.github.com>
2023-02-05 10:19:50 +0800

    Replace the data into the special folder

--- PG/PG15/Day1-Install_single_DB.sh
+++ PG/PG15/Day1-Install_single_DB.sh
@@ -25,15 +25,29 @@ sed -e 's|^mirrorlist=|#mirrorlist=|g' \
 # dnf upgrade
 
 # Install the repository RPM:
-sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
 # Disable the built-in PostgreSQL module:
-sudo dnf -qy module disable postgresql
+dnf -qy module disable postgresql
 
 # Install PostgreSQL:
-sudo dnf install -y postgresql15-server
+dnf install -y postgresql15-server
 
-# Optionally initialize the database and enable automatic start:
+# Place the data into special folder but not the default one 
+mkdir -p /pgsql/15/{data,dump,dba,backup}
+chown -R postgres:postgres /pgsql
+chmod -R 700 /pgsql 
+echo "export PATH=/usr/pgsql-15/bin:$PATH">> /etc/profile
+ 
+echo "export PGHOME=/pgsql/15" >> /var/lib/pgsql/.bash_profile
+echo "export PGDATA=/pgsql/15/data " >> /var/lib/pgsql/.bash_profile
+echo "export PGDUMP=/pgsql/15/dump  " >> /var/lib/pgsql/.bash_profile
+echo "export PGBACKUP=/pgsql/15/backup " >> /var/lib/pgsql/.bash_profile
+
+sed -e 's|Environment=PGDATA=/var/lib/pgsql/15/data/|Environment=PGDATA=/pgsql/15/data/|g' -i.bak /usr/lib/systemd/system/postgresql-15.service
+
+
+# initialize the database and enable automatic start:
 sudo /usr/pgsql-15/bin/postgresql-15-setup initdb
 sudo systemctl enable postgresql-15
 sudo systemctl start postgresql-15
